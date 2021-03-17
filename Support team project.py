5d96b7f7fbdaacd2195a27cc9be133389281b3f2
Rxann <80224521+Rxann@users.noreply.github.com>
2021-03-17 19:38:14 -0400

    Create Support team project.py

---
+++ Support team project.py
@@ -0,0 +1,9 @@
+email = input('What is your email? ')
+name =  input('What is your name? ')
+response = '''   
+Thank You for Contacting Support.
+We will review your problem then email you back.
+Cheers,
+The Support Team
+'''
+print(name + response + 'this is the email we will write back to: ' + email)
