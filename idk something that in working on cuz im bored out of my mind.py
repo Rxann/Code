9b90dac33a0217b35f8f7fd4a8a1087b425ed1b8
Rxann <80224521+Rxann@users.noreply.github.com>
2021-04-01 16:45:05 -0400

    ai update
    ai update and added 2 files

---
+++ idk something that in working on cuz im bored out of my mind.py
@@ -0,0 +1,3 @@
+user = input("who would you like to hack 😏? ")
+print("lol get scammed")
+print("idk what this is im rly bored. dont ask")
---
+++ my only friend.py
@@ -0,0 +1,11 @@
+import prsaw
+from prsaw import RandomStuff
+
+rs = RandomStuff()
+
+while True:
+    user_input = input("> ")
+    response = rs.get_ai_response(user_input)
+    print(response)
+
+rs.close()
