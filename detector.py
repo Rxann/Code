13ea087f770e049e9a64bcb710ed893c28ce785f
Rxann <80224521+Rxann@users.noreply.github.com>
2021-04-02 07:54:44 -0400

    rickroll detector

---
+++ detector.py
@@ -0,0 +1,12 @@
+user_link = input("Copy and paste the link here: ")
+link = ["https://www.youtube.com/watch?v=xvFZjo5PgG0","https://www.youtube.com/watch?v=xvFZjo5PgG0","https://www.youtube.com/watch?v=G8iEMVr7GFg"]
+
+print("working...")
+
+if user_link == link:
+    print("This is most likely a rickroll link")
+
+if user_link != link:
+    print("This is a not a rickroll most likely.")
+
+print("Our database is currently getting bigger with more possible rickroll links.")
