6af443513598539c152e8edd03c9a5ad16154a9b
Rxann <80224521+Rxann@users.noreply.github.com>
2021-04-03 09:30:29 -0400

    some **minor** updates

---
+++ app.py
@@ -0,0 +1,11 @@
+class Point:
+    def move():
+        print("move")
+    def draw():
+        print("draw")
+    def peepee():
+        print("hahah you said peepee")
+    def poopoo():
+        print("bahahahahhaha you said poopoo")
+
+Point.draw()
