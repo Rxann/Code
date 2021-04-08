b5fc2d0e900412490349f218dbe2b83a729a68e8
Rxann <80224521+Rxann@users.noreply.github.com>
2021-04-08 19:04:35 -0400

    general testing updates

--- app.py
+++ app.py
@@ -1,11 +1,3 @@
-class Point:
-    def move():
-        print("move")
-    def draw():
-        print("draw")
-    def peepee():
-        print("hahah you said peepee")
-    def poopoo():
-        print("bahahahahhaha you said poopoo")
+from Pog import Pog
 
-Point.draw()
+Pog.pog("reet")
