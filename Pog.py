655ee7dcc0201dbdd08027521fa7269048379ad5
Rxann <80224521+Rxann@users.noreply.github.com>
2021-04-08 19:04:26 -0400

    Pog module

---
+++ Pog.py
@@ -0,0 +1,10 @@
+class Pog:
+    def pogchamp():
+        print("pogggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggerrrrrrrsssssssssssss")
+    def omegalol():
+        print("omegalol")
+    def send(text):
+        print(text)
+    def pog(pogtext):
+        print(f"{pogtext} has been magicaly pogified")
+        
