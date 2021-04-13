a54f2e8e2a5a62111c85dcf8fe4f6769ca1a9d1b
Rxann <80224521+Rxann@users.noreply.github.com>
2021-04-13 14:37:19 -0400

    unscrapped lol

--- cmd.py
+++ cmd.py
@@ -1,1 +1,18 @@
-# scrapped
+terminal = input("> ")
+help_cmd = "!help"
+yeet_cmd = "!yeet"
+pog_cmd = "!pog"
+spicee = "!spicee"
+wasted = "!wasted"
+while True:
+    if terminal == yeet_cmd:
+        print("yeet")
+    if terminal == help_cmd:
+        print("Help: Commands")
+    if terminal == pog_cmd:
+        print("pog :)")
+    if terminal == spicee:
+        print("Spicee")
+    if terminal == wasted:
+        print("You have been wasted lmao")
+    break
