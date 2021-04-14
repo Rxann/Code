ff56d92b1de6efa785d6fe4c59d31d2e339f103f
Rxann <80224521+Rxann@users.noreply.github.com>
2021-04-14 07:01:24 -0400

    de-encoder now fully functional

--- de-encoder.py
+++ de-encoder.py
@@ -1,4 +1,13 @@
 import dcoder
+option = input("Encode or decode: ")
+aa = "encode"
+ab = "decode"
 msg = input("> ")
-decoder = dcoder.text2caesar(f"{msg}")
-print(decoder)
+encoder = dcoder.text2caesar(f"{msg}")
+decoder = dcoder.caesar2text(f"{msg}")
+
+if option == aa:
+    print(encoder)
+
+if option == ab:
+    print(decoder)
