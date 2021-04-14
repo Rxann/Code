36bfe194d68d9eec828d5e50199c16be33d27c99
Rxann <80224521+Rxann@users.noreply.github.com>
2021-04-14 07:23:41 -0400

    de and encoder

--- de-encoder.py
+++ de-encoder.py
@@ -1,13 +1,18 @@
 import dcoder
-option = input("Encode or decode: ")
+option = input("Encode decode or bin: ")
 aa = "encode"
 ab = "decode"
+ac = "bin"
 msg = input("> ")
 encoder = dcoder.text2caesar(f"{msg}")
 decoder = dcoder.caesar2text(f"{msg}")
-
+binencode = dcoder.text2bin(f"{msg}")
 if option == aa:
     print(encoder)
-
 if option == ab:
     print(decoder)
+
+if option == ac:
+    print(binencode)
+    
+
