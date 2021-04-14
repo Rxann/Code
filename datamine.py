74d28b157ff1aff7a0233f69b08c8ca053218747
Rxann <80224521+Rxann@users.noreply.github.com>
2021-04-14 10:52:29 -0400

    slight **minor** update

--- datamine.py
+++
@@ -1,8 +0,0 @@
-login_try = input("What is the access key: ")
-keys = "6054"
-if keys == login_try:
-        print("Granted")
-        print("mission succesfull")
-        print("All information has been granted above")
-if login_try != keys:
-        print("Access denied. Please restart the program and try again")
